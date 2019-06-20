44791ad4f27ff436851c59690f44a31980457793
Cara Takemoto <cktakemoto@gmail.com>
2019-06-20 16:08:09 -0400

    wrote comments api post route to create a new comment

---
+++ server/src/api/comments.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const db = require('../db');
+module.exports = router;
+
+router.post('/:placeId', async (req, res, next) => {
+  try {
+    const place = await db.models.place.findOne({
+      where: {
+        id: +req.params.placeId,
+      },
+    });
+    if (!place) {
+      next();
+    }
+    const comment = await db.models.comment.create({
+      title: req.body.title,
+      body: req.body.body,
+      userId: req.body.userId,
+    });
+    comment.setPlace(place);
+    res.status(201).json(comment);
+  } catch (err) {
+    next(err);
+  }
+});
