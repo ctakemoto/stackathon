63b46b6c4a0f47a1e8c1f3cdcd84478b3119d92c
Cara Takemoto <cktakemoto@gmail.com>
2019-06-21 17:52:05 -0400

    navbar style changes

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -6,15 +6,18 @@
         <b-nav-item to="/map">Map</b-nav-item>
       </b-navbar-nav>
 
-      <b-navbar-nav v-if="!$store.state.isLoggedIn">
-        <b-nav-item to="/register">Sign Up</b-nav-item>
-      </b-navbar-nav>
-      <b-navbar-nav v-if="!$store.state.isLoggedIn">
-        <b-nav-item to="/login">Login</b-nav-item>
-      </b-navbar-nav>
-      <b-nav-form v-if="$store.state.isLoggedIn">
-        <b-button @click="logout">Logout</b-button>
-      </b-nav-form>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item to="/register" v-if="!$store.state.isLoggedIn">Sign Up</b-nav-item>
+          <b-nav-item to="/login" v-if="!$store.state.isLoggedIn">Login</b-nav-item>
+          <b-nav-text v-if="$store.state.isLoggedIn">{{userGreeting}}</b-nav-text>
+          <b-nav-item v-if="$store.state.isLoggedIn" to="#">Profile</b-nav-item>
+          <b-nav-item v-if="$store.state.isLoggedIn" @click="logout">Logout</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
   </div>
 </template>
@@ -23,6 +26,12 @@
 import AuthenticationService from '../services/AuthenticationService';
 export default {
   name: 'navbar',
+  computed: {
+    userGreeting() {
+      return `Welcome, ${this.$store.state.user.name ||
+        this.$store.state.user.email}`;
+    },
+  },
   methods: {
     async logout() {
       // set toast based on status
