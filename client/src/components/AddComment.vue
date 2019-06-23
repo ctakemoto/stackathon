1ef335d03d71b05a97966e10d9a728dc4d60d69d
Cara Takemoto <cktakemoto@gmail.com>
2019-06-23 10:32:56 -0400

    made addcomment form and added it to placedetail

---
+++ client/src/components/AddComment.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="add-comment">
+    <h3>Add a Review</h3>
+
+    <b-form>
+      <b-form-group id="input-group-1" label="Subject:" label-for="input-1">
+        <b-form-input
+          id="input-1"
+          type="subject"
+          name="subject"
+          placeholder="subject"
+          v-model="subject"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-2" label="Comment:" label-for="input-2">
+        <b-form-textarea
+          id="input-2"
+          type="comment"
+          name="comment"
+          placeholder="What do you think about this location?"
+          v-model="comment"
+          rows="3"
+          max-rows="6"
+          required
+        ></b-form-textarea>
+      </b-form-group>
+
+      <b-button @click.prevent="addComment">Login</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'add-comment',
+  data() {
+    return {
+      subject: '',
+      comment: '',
+    };
+  },
+  methods: {
+    addComment() {},
+  },
+};
+</script>
+
+<style scoped>
+.add-comment {
+  width: 80%;
+  margin: 10px auto;
+}
+.add-comment form {
+  text-align: left;
+  border: 1px solid #c2c2c2;
+  padding: 10px;
+  border-radius: 10px;
+}
+</style>
--- client/src/components/BathroomDetail.vue
+++ client/src/components/BathroomDetail.vue
@@ -8,6 +8,7 @@
         v-bind:mapCoords="this.bathroom.coordinates"
         v-bind:mapHeight="'400px'"
       />
+      <AddComment/>
     </div>
   </b-container>
 </template>
@@ -16,6 +17,7 @@
 import PlacesServices from '../services/PlacesService';
 import Loading from './LoadingSpinner';
 import MapView from './MapView';
+import AddComment from './AddComment';
 export default {
   name: 'bathroom-detail',
   methods: {},
@@ -26,48 +28,54 @@ export default {
       mapErr: false,
     };
   },
+  methods: {
+    async getPlace() {
+      // retrieve data for specific bathroom
+      try {
+        const { data } = await PlacesServices.getPlace(this.$route.params.id);
+
+        this.bathroom = data;
+
+        if (
+          !this.bathroom.address &&
+          this.bathroom.latitude &&
+          this.bathroom.longitude
+        ) {
+          // if it has coords but no address, go fetch address
+          const addressResponse = await PlacesServices.getAddressFromCoords(
+            this.bathroom.latitude,
+            this.bathroom.longitude
+          );
+          this.bathroom.address = addressResponse.data.address;
+        }
+        if (
+          (!this.bathroom.latitude || !this.bathroom.longitude) &&
+          this.bathroom.address
+        ) {
+          const coordsResponse = await PlacesServices.getCoordsFromAddress(
+            this.bathroom.address
+          );
+          this.bathroom.coordinates = coordsResponse.data.coords;
+        }
+        this.$store.dispatch('setBathroom', this.bathroom);
+      } catch (error) {
+        console.error(error);
+        this.$bvToast.toast(error.response.data, {
+          variant: 'danger',
+          title: 'Error finding Coordinates',
+        });
+        this.mapErr = true;
+      }
+      this.isLoading = false;
+    },
+  },
   components: {
     Loading,
     MapView,
+    AddComment,
   },
-  async mounted() {
-    // retrieve data for specific bathroom
-    try {
-      const { data } = await PlacesServices.getPlace(this.$route.params.id);
-
-      this.bathroom = data;
-
-      if (
-        !this.bathroom.address &&
-        this.bathroom.latitude &&
-        this.bathroom.longitude
-      ) {
-        // if it has coords but no address, go fetch address
-        const addressResponse = await PlacesServices.getAddressFromCoords(
-          this.bathroom.latitude,
-          this.bathroom.longitude
-        );
-        this.bathroom.address = addressResponse.data.address;
-      }
-      if (
-        (!this.bathroom.latitude || !this.bathroom.longitude) &&
-        this.bathroom.address
-      ) {
-        const coordsResponse = await PlacesServices.getCoordsFromAddress(
-          this.bathroom.address
-        );
-        this.bathroom.coordinates = coordsResponse.data.coords;
-      }
-      this.$store.dispatch('setBathroom', this.bathroom);
-    } catch (error) {
-      console.error(error);
-      this.$bvToast.toast(error.response.data, {
-        variant: 'danger',
-        title: 'Error finding Coordinates',
-      });
-      this.mapErr = true;
-    }
-    this.isLoading = false;
+  mounted() {
+    this.getPlace();
   },
 };
 </script>
