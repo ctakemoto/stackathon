f657da2d9a6a3d36daa0e154e0d0eb466485c6c0
Cara Takemoto <cktakemoto@gmail.com>
2019-06-24 00:36:32 -0400

    made express server serve up dist folder

--- server/src/app.js
+++ server/src/app.js
@@ -84,7 +84,7 @@ const createApp = () => {
   app.use('/api', require('./api'));
 
   // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', '..', 'client', 'public')));
+  app.use(express.static(path.join(__dirname, '..', '..', 'client', 'dist')));
 
   // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
