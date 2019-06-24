e072898d9cdf44084d51e19066e7e8448860db8c
Cara Takemoto <cktakemoto@gmail.com>
2019-06-24 16:51:19 -0400

    deploy

--- server/src/app.js
+++ server/src/app.js
@@ -56,17 +56,6 @@ const createApp = () => {
   // compression middleware
   app.use(compression());
 
-  // this allows our client to access our api
-  // ** ask about if this is the correct setting
-  app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-  });
-
   // session middleware with passport
   app.use(
     session({
@@ -99,9 +88,7 @@ const createApp = () => {
 
   // sends index.html
   app.use('*', (req, res) => {
-    res.sendFile(
-      path.join(__dirname, '..', '..', 'client', 'public/index.html')
-    );
+    res.sendFile(path.join(__dirname, '..', '..', 'client', 'dist/index.html'));
   });
 
   // error handling endware
