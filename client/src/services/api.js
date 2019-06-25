8779bf487be35eef059449b649821df0d032222b
Cara Takemoto <cktakemoto@gmail.com>
2019-06-24 20:25:14 -0400

    fix api baseURL on client side

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,14 @@
 // to connect to the backend api endpoints
 import axios from 'axios';
 
+const baseURL =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:3000'
+    : 'https://toilt.herokuapp.com/';
 export default () => {
   return axios.create({
     // environmental variables are defined in .env
-    baseURL: process.env.VUE_APP_SERVER_BASE_URL || `http://localhost:3000`,
+    baseURL: baseURL,
   });
 };
 console.log('process.env', process.env);
